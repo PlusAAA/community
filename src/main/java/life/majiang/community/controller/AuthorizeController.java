08ceb6455ffc50b5e80172a595f8d916080ea379
hujinyu <1030422807@qq.com>
2019-11-19 13:27:49 +0800

    insert token

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,15 +2,18 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
-import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,14 +28,29 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirect_uri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO tokenDTO = new AccessTokenDTO(client_id, client_secret, code,
                 redirect_uri,state);
         String accessToken = githubProvider.getAccessToken(tokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        githubUser = new GithubUser("小白白", Integer.toUnsignedLong(1030422807), "我是一只小菜皮卡丘");
+        System.out.println(githubUser.getName());
+        if(githubUser != null){
+            // 登录成功
+            User user = new User(githubUser.getName(), String.valueOf(githubUser.getId()), UUID.randomUUID().toString(),
+                    System.currentTimeMillis(), System.currentTimeMillis());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            // 登录失败
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/life/majiang/community/model/User.java
@@ -0,0 +1,66 @@
+package life.majiang.community.model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String accountId;
+    private String token;
+    private long gmtCreate;
+    private long gmtModified;
+
+    public User(String name, String accountId, String token, long gmtCreate, long gmtModified) {
+        this.name = name;
+        this.accountId = accountId;
+        this.token = token;
+        this.gmtCreate = gmtCreate;
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
